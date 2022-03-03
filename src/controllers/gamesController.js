10acc5e544b88006fd68c4cf425967483d3759ae
Tarcísio F. S. Teles <tfst@poli.br / tfst@ecomp.poli.br>
2022-03-03 13:00:32 -0300

    fix: added join in query 'get /games' to display correct query data

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -2,7 +2,7 @@ import dbConnection from '../database/database.js';
 
 export async function getGames(req, res) {
   try {
-    const gamesPromise = await dbConnection.query('SELECT * FROM games;');
+    const gamesPromise = await dbConnection.query('SELECT games.*, categories.name as "categoryName" FROM games JOIN categories ON games."categoryId"=categories.id;');
     res.status(200).send(gamesPromise.rows);
 
   } catch (error) {
@@ -14,8 +14,8 @@ export async function getGames(req, res) {
 
 export async function insertGame(req, res) {
   try {
-    const newGame = req.body;
-  await dbConnection.query(`INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5);`, [{newGame}]);
+    const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+  await dbConnection.query(`INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5);`, [name, image, stockTotal, categoryId, pricePerDay]);
     res.sendStatus(501);
     
   } catch (error) {
